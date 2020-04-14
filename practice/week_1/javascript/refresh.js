ef6fbebc9b7ecb750945035f80871f9d221c43b7
seleniadelgado <angiedelgado08@gmail.com>
2020-04-14 12:05:58 -0700

    adding today's practice projects

---
+++ practice/week_1/javascript/refresh.js
@@ -0,0 +1,38 @@
+// var myFirstVar = true;
+
+// myFirstVar = null;
+
+// console.log(myFirstVar);
+
+// // if statement
+// if (myFirstVar) {}
+
+
+// //loop statement
+// while(false) {
+// }
+
+// for (var counter =  0; 0 < 10; counter = counter + 1) {
+
+// }
+
+
+// do {executable code} while (condition);
+
+
+// //object
+// var myObject = {
+//     "my": "my value for the key",
+//     "my second key": null
+// };
+
+//function
+function functionName(p1, p2) {
+    return p1 * p2 // the function returns the product of both arguments.
+}
+console.log(functionName(4, 5))
+
+const myFirstConst = 10;
+
+const myConstObject = {};
+myConstObject.firstArgument = "something";
