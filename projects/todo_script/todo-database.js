a8134e862dcbc4a2ff268674b53e17b63d4cfb30
seleniadelgado <angiedelgado08@gmail.com>
2020-05-22 11:28:35 -0700

    changes to questions

---
+++ projects/todo_script/todo-database.js
@@ -0,0 +1,96 @@
+const mongoose = require('mongoose');
+
+const dbURL = "mongodb+srv://User1:userPassword@cluster0-obwhs.mongodb.net/todo-database?retryWrites=true&w=majority";
+const dbOptions = {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+};
+
+mongoose.connect(dbURL, dbOptions, (error) => {
+    if (error) {
+        console.log("Mongoose received an error!", error);
+    } else {
+        console.log("Succesfully connected to MongoDB");
+    }
+});
+
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB Error: "));
+
+let Schema = mongoose.Schema;
+
+//whats allowed in the schema
+let noteSchema = new Schema({
+    note: String,
+    completed_status: Boolean
+});
+let noteModel = new mongoose.model("notes", noteSchema);
+
+if (process.argv[2] === undefined || process.argv[2] === "list") {
+   //listNotes();
+
+} else if (process.argv[2] === "new") {
+   let newNote = {
+       note: process.argv[3],
+       completed_status: false
+   }
+   data.notes.push(newNote);
+   //convert following line to a function.
+   fs.writeFileSync(filename, JSON.stringify(data), "utf8");
+   console.log("Note successfully added!")
+   listNotes();
+
+// } else if (process.argv[2] === "edit") {
+
+//    data.notes[process.argv[3] - 1].note = process.argv[4];
+//    fs.writeFileSync(filename, JSON.stringify(data), "utf8");
+//    console.log("Update Note!")
+//    listNotes();
+
+// } else if (process.argv[2] === "delete") {
+//    if (process.argv[3] <= 0) {
+//        console.log("Hey, use positive numbers!");
+//        return;
+//    }
+//    if (process.argv[3] == null) {
+//        console.log("Hey, type a note number!");
+//        return;
+//    }
+//    if (process.argv[3] == parseFloat(process.argv[3])){
+//        console.log("Hey, don't use decimals!");
+//        return;
+//    }
+//    if (process.argv[3] !== isNaN(process.argv[3])) {
+//        console.log("Hey, use digits!");
+//        return;
+//    }
+//    else {
+//    data.notes.splice(process.argv[3] - 1, 1);
+//    fs.writeFileSync(filename, JSON.stringify(data), "utf8");
+//    console.log("Note Deleted succesfully. = .(");
+//    listNotes();
+//    }
+
+// } else if (process.argv[2] === "done") {
+//    data.notes[process.argv[3] -1].comepleted_status = true;
+//    fs.writeFileSync(filename, JSON.stringify(data), "utf8");
+//    console.log("Note marked as completed! =)");
+// } else {
+//    console.log(`
+//        Welcome to my TODO script! The following commands can be typed after"node todo.js":
+//        1) new: Creates a new note, the argument after should be the note text.
+//        2) edit: edits an existing note, the argument after edit should be the note number, and after that the note text.
+//        3) delete: deleted an existing note, the argument after delete should be the note number.
+//        4) done: marks an existing note as done, the argument after done should be the note number;`);
+// }
+
+// function listNotes() {
+//    for (let i = 0; i < data.notes.length; i++) {
+//        let currentNote = data.notes[i];
+//        //ternary operator is like a 1 line
+//        let status = data.notes[i].completed_status ? "Completed":
+//        console.log(`Note ${i + 1}: ${data.notes[i].note}`);
+//    }
+// }
